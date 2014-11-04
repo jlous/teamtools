b233be2beaa6d6aac5c0aa1cb323c8455fa98fb5
jlous <joachim@lous.org>
2014-11-04 13:08:00 +0100

    Create buildstatus.user.js

---
+++ buildstatus.user.js
@@ -0,0 +1,63 @@
+// ==UserScript==
+// @name         buildstatus
+// @namespace    http://your.homepage/
+// @version      0.1
+// @description  enter something useful
+// @author       You
+// @match        https://code.barentswatch.net/tc/externalStatus.html*
+// @grant        none
+// @require 	 http://code.jquery.com/jquery-2.1.1.min.js
+// ==/UserScript==
+
+function mapUrls(html){
+	return html.replace(/http:\/\/153.44.6.115:8111\//g, "https://code.barentswatch.net/");
+}
+
+function fixCss(){
+    $("<link/>", {
+       rel: "stylesheet",
+       type: "text/css",
+       href: "https://code.barentswatch.net/tc/css/status/externalStatus.css"
+    }).appendTo("head");
+    $("<style type='text/css'>" +
+      "  .tcTable{background: rgba(255, 255, 255, 0.7)} " +
+      "  table.tcTable a{color:white;}" +
+      "  div.teamCityBuildNumber{display: inline;}" +
+      "  div.teamCityDateTime{display: inline;}" +
+      "  table.tcTable td.buildNumberDate div.teamCityDateTime{color:black;}" +
+      "</style>"
+     ).appendTo("head");
+    
+    $("body").css("font-family", "'helvetica neue', arial, sans-serif");
+}
+
+function colorPage(colorName){
+    $("body").css("background-color", colorName);
+}
+
+function highlightStatus(){
+    var errorIcons = $("img[src*='error']");
+    colorPage(errorIcons.length > 0 ? "red" : "green");
+}
+ 
+function render(data){
+    $("body").html(mapUrls(data.toString()));
+    addTimeStamp();
+    highlightStatus();
+}
+
+function refresh(){
+    $.get("/tc/externalStatus.html", render);
+}
+
+function scheduleRefresh(){
+    setInterval(refresh, 10000);
+}
+
+function addTimeStamp(){
+	$("table").append("<tr><td><small>Sist oppdatert " + new Date().toLocaleString() + "</small></td></tr>");
+}
+
+fixCss();
+refresh();
+scheduleRefresh();
